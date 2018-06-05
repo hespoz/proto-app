87b5e828d72ff8c5de676eaf1d34ab9ae2d259a7
Hector Espoz <hespoz@Hectors-MBP.fritz.box>
2018-06-05 23:52:02 +0200

    Work in progress for add new screen

--- src/components/ConfigPanel/ControlConfig/ButtonConfig.js
+++ src/components/ConfigPanel/ControlConfig/ButtonConfig.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react'
 import {connect} from "react-redux"
 
-import { Form, Select, TextArea } from 'semantic-ui-react'
+import { Form, Select, TextArea, Divider, Button, Icon, Header } from 'semantic-ui-react'
 import { updateElementProp, fetchFieldsInScreen } from "../../../actions/canvasAction";
+
+import './ControlConfig.scss'
 
 const options = [
     { key: 'angular', text: 'Angular', value: 'angular' },
@@ -25,6 +27,8 @@ const options = [
     { key: 'ux', text: 'User Experience', value: 'ux' },
 ]
 
+
+
 @connect((store) => {
     return {
         screenList: store.canvas.screenList,
@@ -33,6 +37,10 @@ const options = [
     }
 })
 export default class ButtonConfig extends Component {
+
+    state = {
+        showAddNewState: false
+    }
 
     componentDidMount = () => {
         this.props.dispatch(fetchFieldsInScreen())
@@ -48,56 +56,100 @@ export default class ButtonConfig extends Component {
         })
     }
 
+    addNewState = ()  => {
+        this.setState({showAddNewState: true})
+    }
+
     render() {
         const { selectedElementInfo } = this.props
 
-        console.log('this.props.fieldsCurrentScreen', this.props.fieldsCurrentScreen)
+        if(this.state.showAddNewState){
+            return <div className='content-container'>
+                <header>
+                    <div className='header'>
+                        <div>
+                            <Header as='h3'>New State</Header>
+                        </div>
+                        <div>
+                            <Icon name='close' size='large'/>
+                        </div>
+                    </div>
 
-        return (
-            <div className='content-container'>
-                <Form>
-                    <Form.Field>
-                        <label>{selectedElementInfo.props.goToState.label}</label>
-                        <Select
-                            options={this.getPageList()}
-                            value={selectedElementInfo.props.goToState.value}
-                            onChange={(e, {value}) => {
-                                this.props.dispatch(updateElementProp("goToState", value))
-                            }}
-                        />
-                    </Form.Field>
-                    <Form.Field>
-                        <label>{selectedElementInfo.props.actionName.label}</label>
-                        <input onChange={(e, data) => {
-                            this.props.dispatch(updateElementProp("actionName", e.target.value))
-                        }}
-                               value={selectedElementInfo.props.actionName.value}/>
-                    </Form.Field>
-                    <Form.Field>
-                        <label>{selectedElementInfo.props.actionDescription.label}</label>
-                        <TextArea
-                            onChange={(e, data) => {
-                                this.props.dispatch(updateElementProp("actionDescription", e.target.value))
-                            }}
-                            style={{ minHeight: 100 }}
-                            value={selectedElementInfo.props.actionDescription.value}
-                        />
-                    </Form.Field>
-                    <Form.Field>
-                        <label>{selectedElementInfo.props.actionParameters.label}</label>
-                        <Select
-                            options={this.props.fieldsCurrentScreen}
-                            value={selectedElementInfo.props.actionParameters.value}
-                            multiple
-                            onChange={(e, data) => {
-                                console.log(e.target.value, data)
-                                this.props.dispatch(updateElementProp("actionParameters", data.value))
-                            }}
-                        />
-                    </Form.Field>
-                </Form>
+
+                </header>
+                <segment>
+                    <Form>
+                        <Form.Field>
+                            <label>Screen name</label>
+                            <input/>
+                        </Form.Field>
+                        <Form.Field>
+                            <Button fluid>
+                                New blank page
+                            </Button>
+                            <Divider horizontal>Or</Divider>
+                            <Button fluid>
+                                Clone current screen
+                            </Button>
+                        </Form.Field>
+                    </Form>
+                </segment>
             </div>
-        )
+        } else {
+            return (
+                <div className='content-container'>
+                    <Form>
+                        <Form.Field>
+                            <label>{selectedElementInfo.props.goToState.label}</label>
+                            <Select
+                                options={this.getPageList()}
+                                value={selectedElementInfo.props.goToState.value}
+                                onChange={(e, {value}) => {
+                                    this.props.dispatch(updateElementProp("goToState", value))
+                                }}
+                            />
+
+                            <Divider horizontal>Or</Divider>
+
+                            <Button fluid onClick={this.addNewState}>
+                                New state
+                            </Button>
+
+                        </Form.Field>
+                        <Form.Field>
+                            <label>{selectedElementInfo.props.actionName.label}</label>
+                            <input onChange={(e, data) => {
+                                this.props.dispatch(updateElementProp("actionName", e.target.value))
+                            }}
+                                   value={selectedElementInfo.props.actionName.value}/>
+                        </Form.Field>
+                        <Form.Field>
+                            <label>{selectedElementInfo.props.actionDescription.label}</label>
+                            <TextArea
+                                onChange={(e, data) => {
+                                    this.props.dispatch(updateElementProp("actionDescription", e.target.value))
+                                }}
+                                style={{ minHeight: 100 }}
+                                value={selectedElementInfo.props.actionDescription.value}
+                            />
+                        </Form.Field>
+                        <Form.Field>
+                            <label>{selectedElementInfo.props.actionParameters.label}</label>
+                            <Select
+                                options={this.props.fieldsCurrentScreen}
+                                value={selectedElementInfo.props.actionParameters.value}
+                                multiple
+                                onChange={(e, data) => {
+                                    console.log(e.target.value, data)
+                                    this.props.dispatch(updateElementProp("actionParameters", data.value))
+                                }}
+                            />
+                        </Form.Field>
+                    </Form>
+                </div>
+            )
+        }
+
     }
 
 }
